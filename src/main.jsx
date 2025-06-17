5671ddfa151cb0523838f764c3e54b5023d7d3e9
Abnerpino <abnerpino15@gmail.com>
2025-06-17 10:17:22 -0600

    Se realizaron cambios para embeber el favicon.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import favicon from '../public/iasd.png';
+import './index.css'
+
+const link = document.createElement('link');
+link.rel = 'icon';
+link.href = favicon;
+document.head.appendChild(link);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
