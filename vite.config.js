6f68d2ca434257210161a8fa3686c86f11804287
Abnerpino <abnerpino15@gmail.com>
2025-06-17 10:16:36 -0600

    Se agregaron las configuraciones para que funcione el empaquetado con SigleFile.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    svgr()
+    svgr(),
+    viteSingleFile()
   ],
+  build: {
+    target: 'esnext',
+    assetsInlineLimit: Infinity, // asegura que todas las imágenes y fuentes también se embeban
+  },
 });
